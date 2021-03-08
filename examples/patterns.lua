fc3facf0a5711d738ca9b8cac2ebb201e092e2f6
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2021-03-08 01:30:46 +0100

    Update the style a bit more

--- examples/patterns.lua
+++ examples/patterns.lua
@@ -1,8 +1,8 @@
 use_keymap 'AddictiveDrums2'
 
-pattern:hh      [[********]]; x2()
-pattern:snare   [[--*---*-]]; x2()
-pattern:kick    [[*---*---]]; x2()
+pattern:hh      [[********]] x2()
+pattern:snare   [[--*---*-]] x2()
+pattern:kick    [[*---*---]] x2()
 
 for i = 0, 15 do
     if i % 3 == 0 or i % 7 == 0 then
